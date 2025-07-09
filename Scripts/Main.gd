e939917f9db347f7399b604a6520af64a5531943
9erikSantos <9erikSantos6@gmail.com>
2025-07-09 01:04:59 -0300

    refactor: Organizando cenas de interface e outros...

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -4,4 +4,4 @@ func _ready() -> void:
 	call_deferred("_go_to_main_menu")
 
 func _go_to_main_menu():
-	get_tree().change_scene_to_file("res://Scenes/MainMenu/MainMenu.tscn")
+	get_tree().change_scene_to_file("res://UI/MainMenu.tscn")
